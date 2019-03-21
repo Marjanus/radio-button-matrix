99daf69cf9ded1c7dcc5751c164387c62b131086
Marjanus <martynas.janusevicius@gmail.com>
2019-03-21 22:15:21 +0200

    RBM: add initial config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'dist', 'index.html'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './ index.html',
+        }),
+    ],
+};
